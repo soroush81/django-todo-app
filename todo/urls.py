57b83d793149822310f16677719d5a7ad3960bac
Soroush81@gmail.com <soroush81@gmail.com>
2021-06-21 16:53:54 +0430

    changes

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todo/',views.index)
+    path('todo/',views.index,name='all_todos'),
+    path('todo/<slug:todo_id>', views.todo_details, name='todo_details'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer,CategorySerializer,UserSerializer
@@ -5,15 +6,25 @@ from .models import Category, Todo, User
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
+todo = [
+        {'id':'1','title':'carwash', 'description':'car washing', 'iscompleted': 'true'},
+        {'id':'2','title':'read book', 'description':'read a chapter of a book', 'iscompleted': 'false'}
+    ]
 
 def index(request):
-    todo = [
-        {'title':'carwash', 'description':'car washing', 'iscompleted': 'true'},
-        {'title':'read book', 'description':'read a chpater of a book', 'iscompleted': 'false'}
-    ]
+    
     return render(request,'todo/index.html', {
         'show_todo':True, 
         'todo': todo
+    })
+
+def todo_details(request, todo_id):
+    #selected_todo=[x for x in todo if x.id == todo_id]
+    #selected_todo=[x for x in todo if x.id == todo_id]
+    selected_todo={'title':'carwash', 'description':'I need carwash', 'iscompleted': 'true'}
+    return render(request,'todo/todo_details.html',{
+        'todo_title': selected_todo['title'],
+        'todo_description': selected_todo['description']
     })
 
 # Create your views here.
