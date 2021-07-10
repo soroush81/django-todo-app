7dcfffa1d2e29b77fdf200c239a4d3acf42996ae
Soroush81@gmail.com <soroush81@gmail.com>
2021-07-10 15:58:05 +0430

    changes

--- todo/api.py
+++ todo/api.py
@@ -1,8 +1,10 @@
 from .models import Todo
 from rest_framework import generics, viewsets, permissions
-from .serializers import TodoSerializer,RegisterSerializer, UserSerializer
-from django.contrib.auth.models import User
+from .serializers import TodoSerializer,RegisterSerializer,UserSerializer
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class TodoViewSet(viewsets.ModelViewSet):
     permission_classes =[
@@ -20,12 +22,19 @@ class TodoViewSet(viewsets.ModelViewSet):
 
 
 class RegisterApi(generics.GenericAPIView):
+    permission_classes =[
+        permissions.AllowAny
+    ]
+
     serializer_class = RegisterSerializer
     def post(self, request, *args,  **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        token = TokenObtainPairSerializer.get_token(user)
+        print(token)
         return Response({
+            "token":str(token),
             "user": UserSerializer(user,    context=self.get_serializer_context()).data,
             "message": "User Created Successfully.  Now perform Login to get your token",
-        })
+        }, headers={"token":token})
